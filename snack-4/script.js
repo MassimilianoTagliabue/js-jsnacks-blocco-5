8597a6c1ef0ff52345f59f95f84cc82248706db3
MassimilianoTagliabue <massimilianotagliabue.04@gmail.com>
2024-11-15 13:54:06 +0100

    feat: snack 4

--- snack-4/script.js
+++ snack-4/script.js
@@ -7,3 +7,7 @@ const posts = [
 
 // Crea un nuovo array coi nomi di tutti gli autori
 // Risultato: ['Marco', 'Luca', 'Fabrizio', 'Enrico']
+
+const nomi = posts.map((curItem) => curItem.author)
+
+console.log(nomi);
