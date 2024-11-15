e30039189cc7ac5fde3847722e8ec0cb5465010c
MassimilianoTagliabue <massimilianotagliabue.04@gmail.com>
2024-11-15 14:04:18 +0100

    feat:snack 7

--- snack-7/script.js
+++ snack-7/script.js
@@ -21,3 +21,7 @@ const students = [
 
 // Recupera i dati dello studente con id 2
 // Risultato: { id: 2, name: 'Mario Banfi', age: 34, class: '4A' }
+
+const studente2 = students.filter((curItem) => curItem.id === 2)
+
+console.log(studente2);
